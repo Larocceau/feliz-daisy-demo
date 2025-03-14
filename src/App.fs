946de83b2da11982bbdc053adbb40b1bbe9e8927
Joost Kaptein <joost@compositional-it.com>
2025-03-14 11:20:25 +0000

    minimal starting project

---
+++ src/App.fs
@@ -0,0 +1,10 @@
+﻿module App
+
+open Feliz
+open Browser.Dom
+open Fable.Core.JsInterop
+
+
+
+let root = ReactDOM.createRoot (document.getElementById "root")
+root.render (App.Components.main)
---
+++ src/Components.fs
@@ -0,0 +1,7 @@
+module App.Components
+
+open Feliz
+open Fable.Core
+open Fable.Core.JsInterop
+
+let main = Html.p "Hello world"
